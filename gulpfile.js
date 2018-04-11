5282c7d2e10278230d01df76596bd04cbad7ce02
Vladimir Ivanin <ivaninvv@mail.ru>
2018-04-11 15:34:14 +0300

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,32 @@ var insalesUp = require('insales-uploader');
 var options = require('./options');
 var autoprefixer = require('insales-uploader-autoprefixer')
 var imagemin = require('gulp-imagemin');
+var bs = require('browser-sync').create('insales_server');
 
 var IU = new insalesUp(options)
 
 gulp.task('download', function(){
   return IU.download()
+});
+
+gulp.task('downloadTemplates', function(){
+  return IU.downloadTemplates()
+});
+
+gulp.task('downloadSnippets', function(){
+  return IU.downloadSnippets()
+});
+
+gulp.task('downloadConfig', function(){
+  return IU.downloadConfig()
+});
+
+gulp.task('downloadMedia', function(){
+  return IU.downloadMedia()
+});
+
+gulp.task('downloadFiles', function(){
+  return IU.downloadFiles()
 });
 
 gulp.task('pull', function(){
@@ -18,9 +39,22 @@ gulp.task('push', function(){
   return IU.upload()
 });
 
+gulp.task('uploadFiles', function(){
+  return IU.uploadFiles()
+});
+
+var optionDefault = {
+  proxy: IU.options.themeUrl.replace('https', 'http'),
+  serveStatic: [IU.options.theme.root],
+  reloadDebounce: 5000,
+  reloadDelay: 2000,
+  https: false
+};
+
 gulp.task('stream', function(){
+  bs.init(optionDefault);
   return IU.stream(function () {
-  	// Здесь можно юзать browsersync
+  	bs.reload();
   })
 })
 
